cb831656ec80fd62b01fb49c5e8f924f647e8bd7
Geoff Ford <46463482+g-ford@users.noreply.github.com>
2024-12-04 08:42:59 +1030

    Day 3 🎄

--- 2024/day03/main.py
+++ 2024/day03/main.py
@@ -1,24 +1,43 @@
 from utils import log_time
-
+import re
 
 @log_time
 def part1(parts):
-    pass
+    """Sum all the products together"""
+    return sum(a * b for a, b in parts)
 
 
 @log_time
 def part2(parts):
-    pass
+    """Sum all the products together, but only if we are in a 'do()' block"""
+    summing = True
+    result = 0
+    for op in parts:
+        if op[0] == "do()":
+            summing = True
+        elif op[0] == "don't()":
+            summing = False
+        elif summing: # assume it is a mul() operation
+            result += int(op[1]) * int(op[2])
+    return result
 
 
 @log_time
 def parse_input(lines):
-    pass
+    """Parse the input into a list of tuples of ints"""
+    ops = re.findall(r"mul\((\d+),(\d+)\)", lines)
+    return [(int(a), int(b)) for a, b in ops]
 
+
+@log_time
+def parse_input2(lines):
+    """Parse the input into a list of operations and arguments"""
+    ops = re.findall(r"(mul\((\d+),(\d+)\)|don't\(\)|do\(\))", lines)
+    return ops
 
 lines = open("day03/input.txt", encoding="utf8").read()
-
 parts = parse_input(lines)
+parts2 = parse_input2(lines)
 
 print("Part 1: ", part1(parts))
-print("Part 2: ", part2(parts))
+print("Part 2: ", part2(parts2))
